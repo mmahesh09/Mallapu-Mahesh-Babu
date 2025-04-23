8717d4222d484634577657e07b0af5cc68d62f47
Mahesh <maheshmallapu2309@gmail.com>
2025-04-24 00:54:01 +0530

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './sections/Hero'
+import ShowcaseSection from './sections/ShowcaseSection'
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+      <ShowcaseSection/>
+    </div>
+  )
+}
+
+export default App
